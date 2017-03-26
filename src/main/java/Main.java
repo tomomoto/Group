4076bce35bb995f752c34e7bf156364ebd41019e
Anton Chernyshov <AChernyshov@luxoft.com>
2017-03-26 17:14:17 +0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,4 @@
-import model.TripleLong;
-import process.Processor;
 import utils.BigBossLivesHere;
-import utils.SmallParser;
-
-import java.io.*;
-import java.util.Collections;
 
 /**
  * Created by Tom on 25.03.2017.
